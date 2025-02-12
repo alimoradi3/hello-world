27e6ac4122d84480134a19f764bdf9dff242531b
alimoradi3 <alimoradiqr@gmail.com>
2025-02-12 21:39:55 +0330

    first commit

---
+++ main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        System.err.println("hello world!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+}
