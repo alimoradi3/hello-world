dcfd0e3a067212a75dd2cfeab0a9cc18e5d7c85d
alimoradi3 <alimoradiqr@gmail.com>
2025-02-12 22:55:05 +0330

    im-lement add in local repo

--- main.java
+++ main.java
@@ -10,7 +10,12 @@ class Main {
     public int sub(int a, int b) {
         return a - b;
     }
-    public int mul(int a, int b){
-        return a * b ;
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
     }
 }
